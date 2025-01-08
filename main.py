8a5555ffd65c7866c09805c34ea3e7644c1c8d3e
Amin Javan <81846396+MrJavan@users.noreply.github.com>
2025-01-08 19:54:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 
 def f(n):
-    if n < 0:
-        return "Not valid!"        
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n <= 1:
+        return n
     else:
         return f(n - 2) + f(n - 1)
 
